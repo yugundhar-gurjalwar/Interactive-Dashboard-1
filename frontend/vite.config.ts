933538d8ad217009dd93e3e908cae6a356384496
Yugandhar <yug.gurjalwar@gmail.com>
2026-02-20 16:24:17 +0530

    deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/web-project/",
+  base: "/Interactive-Dashboard-1/",
   build: {
+    outDir: "../docs",
+    emptyOutDir: true,
     sourcemap: true,
   },
 })
